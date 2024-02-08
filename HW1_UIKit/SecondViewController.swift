25e19446d43ca0a24b727858b4360ac3fd000fdc
Michanco <michanco80@gmail.com>
2024-02-08 00:10:25 -0800

    Initial Commit

---
+++ HW1_UIKit/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  HW1_UIKit
+//
+//  Created by Michanco Slesarev on 07.02.2024.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        title = "SecondViewController"
+    }
+}
